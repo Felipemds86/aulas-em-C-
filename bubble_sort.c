29e61cbb23a03a159023a98a0b9a11aff52cba52
Felipe Matheus de Souza <felipemds86@hotmail.com>
2021-09-22 09:52:01 -0300

    Add files via upload

---
+++ bubble_sort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void){
+
+void bubble_sort (int vetor[], int n) {
+    int i,contador,Tam
+
+    printf("digite a quantidade de vetores: []");
+    scanf("%d", Tam)
+
+  
+for(contador =1; contador <Tam; contador++){
+    for(i=0; i< Tam -1, i++){
+        if(numeros[i]> numeros[i+1]){
+            aux=numeros[i];
+            numeros[i] = numeros[i+1];
+            numeros[i+1]= aux;
+
+getch();
+    
+        }
+    }
+  }
+}   
+    
